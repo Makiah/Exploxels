67d8be3efcb2dd19246fd96eed3dac24a325e6ff
Makiah Bennett <makiahtbennett@gmail.com>
2015-12-01 17:38:24 -0500

    Yay!  The sun and moon are now different objects (which simplifies a lot), and rotate around in a perfect ellipse.  Go math!

--- TDP - Source/Assets/Scripts/Other/TimeIndicator.cs
+++ TDP - Source/Assets/Scripts/Other/TimeIndicator.cs
@@ -12,96 +12,96 @@ public class TimeIndicator : MonoBehaviour {
 	}
 
 	/*************** SCRIPT ***************/
-	[SerializeField] private Sprite sun = null;
-	[SerializeField] private Sprite moon = null;
 	private Camera mainCamera;
-
-	private Light mainLight;
-	private Light sunlight;
-
-	private Transform sunAndMoonTransform;
-	private SpriteRenderer currentSprite;
-
 	private Transform player;
 
-	float sunBrightness = 1;
+	//Main directional light.  
+	private Light mainLight;
 
+	//Sun
+	private Transform sunTransform;
+	private Light sunlight;
 	float initialSunlightIntensity = 0;
 
+	private Transform moonTransform;
+	private Light moonlight;
+	float initialMoonlightIntensity = 0;
+
+	//This is multiplied with the player's y coordinate.  
+	float sunBrightnessCoefficient = 1;
+
 	void InitializeTimeIndicationSystem() {
+		//Get Player
+		player = CurrentLevelVariableManagement.GetPlayerReference ().transform;
 		//Get the camera.  
 		mainCamera = transform.parent.GetComponent <Camera> ();
-		sunAndMoonTransform = transform.FindChild ("Sun");
-		currentSprite = sunAndMoonTransform.GetComponent <SpriteRenderer> ();
+		//Get main directional light.  
 		mainLight = GetComponent <Light> ();
-		sunlight = sunAndMoonTransform.FindChild ("Sunlight").GetComponent <Light> ();
+		//Get sun  
+		sunTransform = transform.FindChild ("Sun");
+		sunlight = sunTransform.FindChild ("Sunlight").GetComponent <Light> ();
 		initialSunlightIntensity = sunlight.intensity;
+		//Get moon
+		moonTransform = transform.FindChild ("Moon");
+		moonlight = moonTransform.FindChild ("Moonlight").GetComponent <Light> ();
+		initialMoonlightIntensity = moonlight.intensity;
 
-		//Move this object to the left side of the camera.  This might not be necessary.  
-		transform.position = mainCamera.ScreenToWorldPoint (new Vector3 (0, mainCamera.orthographicSize / 2f, 0));
-
-		player = CurrentLevelVariableManagement.GetPlayerReference ().transform;
-		StartCoroutine ("ManageInGameLighting");
-
-		if (sun != null && moon != null) {
-			StartCoroutine("ControlSunAndMoonLighting");
-		} else {
-			Debug.LogError("No sun and moon are specified!");
-		}
+		StartCoroutine("ControlSunAndMoonLighting");
 	}
 
 	//Manages the lighting depending on the sun and moon's position in the sky.  
 	IEnumerator ControlSunAndMoonLighting() {
-		bool usingSun = true;
-		currentSprite.sprite = sun;
-		float movingRate = .4f;
+		//Controls the sun's speed.  
+		float movingRate = .35f;
 		float customTime = 0;
-		while (true) {
 
+		//Continuously
+		while (true) {
 			//Aspect ratio is width over height.  
 			//This may change at some points (resizing window) so it is calculated again every frame.  
-			float cameraWidth = mainCamera.orthographicSize * 2 * mainCamera.aspect;
+			float cameraHeight = mainCamera.orthographicSize * 2;
+			float cameraWidth = cameraHeight * mainCamera.aspect;
 
 			//Custom clock that moves from 0 to 1 given the moving rate.  .01 is just a constant for convenience.  
 			customTime += Time.deltaTime * .01f * movingRate;
 			//Moves the sun and moon.  
-			sunAndMoonTransform.localPosition = new Vector3(cameraWidth * customTime, sunAndMoonTransform.localPosition.y, sunAndMoonTransform.localPosition.z);
+			sunTransform.localPosition = new Vector3(cameraWidth * customTime, sunTransform.localPosition.y, sunTransform.localPosition.z);
 
 			//The sun and moon should provide a brightness of a sine wave (brightest in the afternoon, neutral at dawn and dusk, and black at midnight).
 			//This function has a period of 1.  (Period is 2pi/coefficient of x).  
 			//This function oscillates the sunPositionCoefficient between 0.2 and 1.  
-			sunBrightness = 0.4f * Mathf.Sin(2 * Mathf.PI * customTime) + .6f;
+			sunBrightnessCoefficient = 0.4f * Mathf.Sin(2 * Mathf.PI * customTime) + .6f;
 
-			//Move to the other side if the sun/moon has reached the opposite end.  
-			if (customTime >= 0.5f && usingSun) {
-				usingSun = false;
-				//Ternary operator
-				currentSprite.sprite = moon;
-				sunlight.intensity = initialSunlightIntensity / 3f;
-			}
+			//Parametric equations for an ellipse.
+			//Where cameraWidth is the length of half of the ellipse, and customTime * 2pi is the period.  
+			//It has to be flipped (goes counterclockwise otherwise).  Multiplied by 1.2 so the sun goes off the screen to begin with.  
+			float x = -1 * (0.5f * cameraWidth * 1.2f) * Mathf.Cos (customTime * 2 * Mathf.PI);
+			//Where 3 is half of the height of the ellipse.  
+			float y = 2 * Mathf.Sin (customTime * 2 * Mathf.PI);
 
-			//Switch back to the sun when the moon has passed beyond the camera.  
-			if (customTime >= 1 && usingSun == false) {
-				usingSun = true;
-				currentSprite.sprite = sun;
+			//Sun movement and brightness
+			sunTransform.localPosition = new Vector3 (x, y, sunTransform.transform.localPosition.z);
+			//Turn off the brightness if behind clouds.  
+			if (sunTransform.localPosition.y >= 0 && sunlight.intensity != initialSunlightIntensity) {
 				sunlight.intensity = initialSunlightIntensity;
-				customTime = 0;
+			} else if (sunTransform.localPosition.y < 0 && sunlight.intensity != 0) {
+				sunlight.intensity = 0;
 			}
 
-			yield return null;
-		}
-	}
-	
-	//This co-routine manages the ambient light present in the scene, thus making exploration deep underground more realistic.  However, it is 
-	//VERY processor-intensive, and I am concerned that an area light may have to be employed instead.  
-	//Note: Color declarations must be normalized (all parameters between 0 and 1).  
-	IEnumerator ManageInGameLighting() {
-		while (true) {
+			//Moon movement and brightness
+			moonTransform.localPosition = new Vector3 (-x, -y, moonTransform.transform.localPosition.z);
+			//Turn off the brightness if behind clouds.  
+			if (moonTransform.localPosition.y >= 0 && moonlight.intensity != initialMoonlightIntensity) {
+				moonlight.intensity = initialMoonlightIntensity;
+			} else if (moonTransform.localPosition.y < 0 && moonlight.intensity != 0) {
+				moonlight.intensity = 0;
+			}
+
 			//Lighting calculations.  
 			float desiredIntensity = 1f / (Mathf.Sqrt(Mathf.Abs (Mathf.Clamp(player.position.y / 8f, -2500, -1))));
-			mainLight.intensity = desiredIntensity * sunBrightness;
+			mainLight.intensity = desiredIntensity * sunBrightnessCoefficient;
+
 			yield return null;
 		}
 	}
-
 }
