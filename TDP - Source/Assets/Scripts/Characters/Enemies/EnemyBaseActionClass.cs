ab4b776acd3740e076a6da9f6f49cffd1e4a4d9e
Makiah Bennett <makiahtbennett@gmail.com>
2015-12-03 16:02:52 -0500

    Added a lame attack animation for the tiger (meh).  Also deleted the unnecessary old coroutine.

--- TDP - Source/Assets/Scripts/Characters/Enemies/EnemyBaseActionClass.cs
+++ TDP - Source/Assets/Scripts/Characters/Enemies/EnemyBaseActionClass.cs
@@ -46,94 +46,7 @@ public abstract class EnemyBaseActionClass : CharacterBaseActionClass {
 		StartCoroutine ("EnemyControl");
 	}
 
-	protected virtual IEnumerator BasicEnemyControl() {
-		while (true) {
-
-			if (Vector2.Distance(transform.position, player.transform.position) <= playerViewableThreshold) {
-
-				Debug.Log(gameObject.name + " can see the player.");
-
-				Stop ();
-
-				float distanceFromLeftPointX = Mathf.Abs(transform.position.x - (player.transform.position.x - ignorePlayerMovementThreshold));
-				float distanceFromRightPointX = Mathf.Abs(transform.position.x - (player.transform.position.x + ignorePlayerMovementThreshold));
-
-				if (distanceFromLeftPointX <= distanceFromRightPointX) {
-					//Flip if some point to the left of the player is further right than the skeleton.  
-					if (player.position.x - ignorePlayerMovementThreshold >= transform.position.x && GetFacingDirection() == -1) {
-						Flip ();
-					} else if (player.position.x - ignorePlayerMovementThreshold >= transform.position.x && GetFacingDirection() == 1) {
-						Flip();
-					} else {
-						if (Mathf.Abs(rb2d.velocity.x) < 0.6f && grounded) {
-							Debug.Log("Jumped");
-							InitializeJump(1);
-							yield return new WaitForSeconds(0.3f);
-						}
-					}
-				} else {
-					//You should not jump if you are going to change direction.  
-					//If facing right and the player is farther right than the attack distance
-					if (player.position.x - transform.position.x >= ignorePlayerMovementThreshold && GetFacingDirection() == 1) {
-						Flip ();
-					} else if (player.position.x + transform.position.x >= ignorePlayerMovementThreshold && GetFacingDirection() != -1) {
-						Flip ();
-					} else {
-						//Jump if the enemy is not going to change direction.  
-						if (Mathf.Abs(rb2d.velocity.x) < 0.3f && grounded) {
-							Debug.Log("Jumped");
-							InitializeJump(1);
-						}
-					}
-				}
-					
-				//Check if the current position is within the boundaries of the attack zone, and the y value is acceptable.  
-				if ((distanceFromLeftPointX <= playerAttackDistance || distanceFromRightPointX <= playerAttackDistance) && (Mathf.Abs(player.transform.position.y - transform.position.y) <= maxYValueSeparation)) {
-					if (distanceFromLeftPointX <= distanceFromRightPointX) {
-						Stop ();
-						FlipToFacePlayer();
-						Attack ();
-						yield return new WaitForSeconds(2.5f);
-					} else {
-						Stop ();
-						FlipToFacePlayer();
-						Attack ();
-						yield return new WaitForSeconds(2.5f);
-					}
-				} else {
-					//Speed management.  
-					Debug.Log("Moving");
-					anim.SetFloat("Speed", 1);
-					rb2d.velocity = new Vector3(moveForce * GetFacingDirection(), rb2d.velocity.y, 0);
-					yield return new WaitForSeconds(3f);
-				}
-
-			} else {
-				//Remain stopped, if not within the viewable radius.  
-				Stop();
-				yield return new WaitForSeconds(1);
-			}
-			
-		}
-
-	}
-
 	protected virtual IEnumerator EnemyControl() {
-		//PSEUDOCODE
-		//Check to make sure the enemy can see the player.  
-			//If it can, check whether it is in the safe zone.  
-				//Flip to face the player and attack.  
-				//Wait a couple seconds.  
-			//Otherwise, flip toward the direction of the nearest safe zone (On either side of the player)
-				//Start moving.  
-				//Wait for a second.  
-				//If the velocity is still 0, jump.  
-					//Wait for part of a second.  
-					//Move again.  
-					//Wait for part of a second.  
-		//Otherwise stop moving.  
-
-		//ACTUAL CODE
 
 		//Continuously
 		while (true) {
@@ -149,8 +62,12 @@ public abstract class EnemyBaseActionClass : CharacterBaseActionClass {
 					//Flip to face the player and attack.  
 					Stop();
 					FlipToFacePlayer();
-					Attack ();
-					Debug.Log("Attacking");
+					//Attack if the y values between the enemy and the player are close enough.  
+					if (Mathf.Abs(player.transform.position.y - transform.position.y) <= maxYValueSeparation) {
+						anim.SetTrigger("Attack");
+						Attack ();
+						Debug.Log("Attacking");
+					}
 					yield return new WaitForSeconds(1.5f);
 
 				} else {
