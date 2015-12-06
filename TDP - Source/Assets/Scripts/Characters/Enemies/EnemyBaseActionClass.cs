37d77d13882a7835161c4618eca6bd7f040fa1db
Makiah Bennett <makiahtbennett@gmail.com>
2015-12-05 22:20:19 -0500

    Fixed a couple errors with the AI/colliders.  Also added Old Man and Hunter NPC to ObjectReferences.

--- TDP - Source/Assets/Scripts/Characters/Enemies/EnemyBaseActionClass.cs
+++ TDP - Source/Assets/Scripts/Characters/Enemies/EnemyBaseActionClass.cs
@@ -104,7 +104,7 @@ public abstract class EnemyBaseActionClass : CharacterBaseActionClass {
 					yield return StartCoroutine(MaintainAConstantXVelocity(GetFacingDirection() * moveForce, 0.3f));
 
 					//In the event that the x velocity is very small, jump.  
-					if (rb2d.velocity.x < moveForce / 1000f && grounded) {
+					if (Mathf.Abs (rb2d.velocity.x) < moveForce / 100f && grounded) {
 						InitializeJump(1);
 						//Wait until we are in the air.  
 						//At some point, consider calculating the time at which the jump is at it's highest point and then resuming, as opposed to some constant.  
--- TDP - Source/Assets/Scripts/Characters/Enemies/Quadripeds/MammothScript.cs
+++ TDP - Source/Assets/Scripts/Characters/Enemies/Quadripeds/MammothScript.cs
@@ -19,8 +19,34 @@ public class MammothScript : EnemyBaseActionClass {
 	}
 
 	protected override void Attack() {
-
+		AttemptToAttackAfterCompletedAnimation ();
+		Debug.Log ("Mammoth is attacking");
 	}
-
-
+	
+	//Based off of SwordScript
+	private float enemyWithinAreaBounds = 4f;
+	private float yOffsetToEnemy = -1f;
+	private float distToEnemyLength = 1.4f;
+	
+	public float mammothAttackPower;
+	
+	void AttemptToAttackAfterCompletedAnimation () {
+		ActionsAfterAnimation += AttackEnemyInFocus;
+	}
+	
+	void AttackEnemyInFocus () {
+		//Use the RaycastAttackUtilites class.  
+		CharacterHealthPanelManager resultingHealthPanelManager = RaycastAttackUtilities.LookForEnemyViaLinecast (transform.position, 
+		                                                                                                          distToEnemyLength, 
+		                                                                                                          yOffsetToEnemy, 
+		                                                                                                          enemyWithinAreaBounds, 
+		                                                                                                          GetFacingDirection (), 
+		                                                                                                          false);
+		
+		if (resultingHealthPanelManager != null) {
+			resultingHealthPanelManager.YouHaveBeenAttacked (mammothAttackPower);
+		}
+		
+	}
+	
 }
