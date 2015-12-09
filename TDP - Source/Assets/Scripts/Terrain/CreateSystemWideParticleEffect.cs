cb38b91a10fda816ede77304de9bc9514354bc86
Makiah Bennett <makiahtbennett@gmail.com>
2015-12-09 17:38:28 -0500

    Colliders now work with the particle effect, although not perfectly.  Has to be on low collision quality or the game crashes.

--- TDP - Source/Assets/Scripts/Terrain/CreateSystemWideParticleEffect.cs
+++ TDP - Source/Assets/Scripts/Terrain/CreateSystemWideParticleEffect.cs
@@ -12,26 +12,19 @@ public class CreateSystemWideParticleEffect : MonoBehaviour {
 		LevelEventManager.InitializeSystemWideParticleEffect -= InitializeSystemWideParticleEffect;
 	}
 
-	[SerializeField] private GameObject snowParticleEffect = null;
-
 	//Actual script
 	void InitializeSystemWideParticleEffect() {
 		if (CurrentLevelVariableManagement.GetMainGameData ().currentLevel == 0) {
 			//Get level length
-			Camera mainCamera = CurrentLevelVariableManagement.GetMainCameraReference().GetComponent <Camera> ();
-			float cameraWidth = (mainCamera.orthographicSize * 2 * mainCamera.aspect) * 2; //Multiply by two so more is covered.  
+			float currentLevelLength = CurrentLevelVariableManagement.GetLevelLengthX();
 
-			//Instantiate the particle effect into the maze.  
-			GameObject createdParticleEffect = (GameObject) (Instantiate(snowParticleEffect, Vector3.zero, Quaternion.identity));
-			//A local x of 0 means in the center of the maze.  
-			//Set particle system size.  The particle system size is changed as the scale changes.  Add 50 so it covers the start segments as well.   
-			createdParticleEffect.transform.localScale = new Vector3 (cameraWidth + 70, 1, 1);
+			//Set snow particle system size.  
+			transform.localScale = new Vector3 (1.2f * currentLevelLength, 1, 1);
+			transform.position = new Vector3 (currentLevelLength / 2, 30, 0);
 
-			//Other setup stuff.  
-			createdParticleEffect.transform.eulerAngles = new Vector3(0, 0, 180);
-			//Bugfix
-			createdParticleEffect.SetActive(false);
-			createdParticleEffect.SetActive(true);
+			//Quick bug fix.  
+			gameObject.SetActive (false);
+			gameObject.SetActive (true);
 
 		}
 	}
