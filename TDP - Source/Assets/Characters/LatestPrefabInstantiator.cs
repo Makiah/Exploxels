f380e4f4c7f9ed254e949d0ef65f82747723e570
Makiah <makiahtbennett@gmail.com>
2016-03-15 11:48:46 -0400

    Added the snowball, but I think that I will not end up using it.  Also changed all of the soldiers on the boss segment to instantiators, so that they are updated along with the changes that have been made to previous soldiers.
    Since the Platformer Art Deluxe Pack has igloo images, why not use the igloo as Thuk's home?  Make a couple for the people who sell stuff, and others for soldiers that lock the door and fight you.  Thuk's can be colored differently to emphasize that it is not the same.
    
    Terrain Concept: Player kind of has to go a roundabout way (on purpose).
    
    --------------          -----  /
                   \              /
                     \           /
                       \--------/

---
+++ TDP - Source/Assets/Characters/LatestPrefabInstantiator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LatestPrefabInstantiator : MonoBehaviour {
+
+	//The object.  
+	[SerializeField] private GameObject prefab;
+
+	//Start at the Start() method.  
+	void Start() {
+		if (prefab != null) {
+			//Create the prefab with all of the properties of this item.  
+			GameObject instantiatedPrefab = (GameObject) (Instantiate(prefab, Vector2.zero, Quaternion.identity));
+			if (transform.parent != null)
+				instantiatedPrefab.transform.SetParent (transform.parent);
+			instantiatedPrefab.transform.localPosition = transform.localPosition;
+			instantiatedPrefab.transform.localRotation = transform.localRotation;
+			instantiatedPrefab.transform.localScale = transform.localScale;
+			//Get rid of the instantiator.  
+			Destroy (gameObject);
+		} else {
+			Debug.Log ("Unable to instantiate an item that does not exist for object " + gameObject.name);
+		}
+	}
+
+}
