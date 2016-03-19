6ceb77e00d0dcd25965cb5a49042cf42ecd24c05
Makiah <makiahtbennett@gmail.com>
2016-03-19 17:41:58 -0400

    Changed the way that the camera moves around the screen based on the mouse and player position.  Works WAY better and more effectively than previously.  Also cool because the previous commit and this commit were done on different laptops during the same timeframe.

--- TDP - Source/Assets/System Scripts/CameraControl.cs
+++ TDP - Source/Assets/System Scripts/CameraControl.cs
@@ -45,17 +45,25 @@ public class CameraControl : MonoBehaviour {
 		while (true) {
 			//The Screen point is remaining constant, yet the world point is changing as the object moves.  
 
+			//The required variables.  
+			Vector3 mousePosition, optimalCameraPosition;
+			float speed;
+
+			//As long as the camera movement is enabled (could be disabled by pressing 'c'.  
 			if (enable) {
+				//Get the mouse and calculate the optimal camera position given the current mouse position.  
+				mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+				optimalCameraPosition = (playerTransform.position + mousePosition) / 2;
+				optimalCameraPosition = new Vector3 (optimalCameraPosition.x, optimalCameraPosition.y, -10);
 
-				Vector3 mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-				Vector3 optimalCameraPosition = (playerTransform.position + mousePosition) / 2 + new Vector3 (0, 0, -10);
+				//The camera should move less quickly the further it gets away from the player.  The camera has a base movement speed of 30.  
+				speed = moveSpeed * Time.deltaTime * (30f / (Vector2.Distance (mousePosition, playerTransform.position) + 1));
 
-				float speed = moveSpeed * Time.deltaTime * Mathf.Abs (transform.position.magnitude - optimalCameraPosition.magnitude);
-
+				//Set the position of the camera based of off the other variables which had already been calculated.  
 				transform.position = Vector3.MoveTowards (transform.position, optimalCameraPosition, speed);
-
 			}
 
+			//Wait one frame.  
 			yield return null;
 		}
 	}
